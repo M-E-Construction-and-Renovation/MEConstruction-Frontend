9af612ae1eaf054af735c1094475bb9754a6144f
Xyron Uy <xyronuysoftware@gmail.com>
2025-12-19 18:55:24 +0800

    Version 9, added more vanity lights and mirror products

--- src/data/products/vanityLights.js
+++ src/data/products/vanityLights.js
@@ -40,6 +40,51 @@ export const lightsCategories = [
         id: "vl3",
         name: "Vanity Light 3",
         tiers: {
+          basic: ["White"],
+        },
+        displayByColor: {
+          White: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/basic-3.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/basic-3.png",
+          },
+        },
+      },
+      {
+        id: "vl4",
+        name: "Vanity Light 4",
+        tiers: {
+          basic: ["White"],
+        },
+        displayByColor: {
+          White: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/basic-4.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/basic-4.png",
+          },
+        },
+      },
+      {
+        id: "vl5",
+        name: "Vanity Light 5",
+        tiers: {
+          basic: ["White"],
+        },
+        displayByColor: {
+          White: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/basic-5.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/basic-5.png",
+          },
+        },
+      },
+      {
+        id: "vl6",
+        name: "Vanity Light 6",
+        tiers: {
           standard: ["Brown"],
         },
         displayByColor: {
@@ -52,8 +97,38 @@ export const lightsCategories = [
         },
       },
       {
-        id: "vl4",
-        name: "Vanity Light 4",
+        id: "vl7",
+        name: "Vanity Light 7",
+        tiers: {
+          standard: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/standard-2.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/standard-2.png",
+          },
+        },
+      },
+      {
+        id: "vl8",
+        name: "Vanity Light 8",
+        tiers: {
+          standard: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/standard-3.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/standard-3.png",
+          },
+        },
+      },
+      {
+        id: "vl9",
+        name: "Vanity Light 9",
         tiers: {
           premium: ["Brown"],
         },
@@ -67,8 +142,8 @@ export const lightsCategories = [
         },
       },
       {
-        id: "vl5",
-        name: "Vanity Light 5",
+        id: "vl10",
+        name: "Vanity Light 10",
         tiers: {
           premium: ["Brown"],
         },
@@ -78,6 +153,36 @@ export const lightsCategories = [
               "/configurator/products/vanity-lights/design/premium-2.png",
             designDisplay:
               "/configurator/products/vanity-lights/design/premium-2.png",
+          },
+        },
+      },
+      {
+        id: "vl11",
+        name: "Vanity Light 11",
+        tiers: {
+          premium: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/premium-3.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/premium-3.png",
+          },
+        },
+      },
+      {
+        id: "vl12",
+        name: "Vanity Light 12",
+        tiers: {
+          premium: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-lights/design/premium-4.png",
+            designDisplay:
+              "/configurator/products/vanity-lights/design/premium-4.png",
           },
         },
       },
--- src/data/products/vanityMirrors.js
+++ src/data/products/vanityMirrors.js
@@ -25,6 +25,36 @@ export const mirrorCategories = [
         id: "vm2",
         name: "Vanity Mirror 2",
         tiers: {
+          basic: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-mirrors/design/basic-2.png",
+            designDisplay:
+              "/configurator/products/vanity-mirrors/design/basic-2.png",
+          },
+        },
+      },
+      {
+        id: "vm3",
+        name: "Vanity Mirror 3",
+        tiers: {
+          basic: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-mirrors/design/basic-3.png",
+            designDisplay:
+              "/configurator/products/vanity-mirrors/design/basic-3.png",
+          },
+        },
+      },
+      {
+        id: "vm4",
+        name: "Vanity Mirror 4",
+        tiers: {
           standard: ["Brown"],
         },
         displayByColor: {
@@ -33,6 +63,36 @@ export const mirrorCategories = [
               "/configurator/products/vanity-mirrors/design/mirror-2.png",
             designDisplay:
               "/configurator/products/vanity-mirrors/design/mirror-2.png",
+          },
+        },
+      },
+      {
+        id: "vm5",
+        name: "Vanity Mirror 5",
+        tiers: {
+          standard: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-mirrors/design/standard-2.png",
+            designDisplay:
+              "/configurator/products/vanity-mirrors/design/standard-2.png",
+          },
+        },
+      },
+      {
+        id: "vm6",
+        name: "Vanity Mirror 6",
+        tiers: {
+          premium: ["Brown"],
+        },
+        displayByColor: {
+          Brown: {
+            productDisplay:
+              "/configurator/products/vanity-mirrors/design/premium-1.png",
+            designDisplay:
+              "/configurator/products/vanity-mirrors/design/premium-1.png",
           },
         },
       },
