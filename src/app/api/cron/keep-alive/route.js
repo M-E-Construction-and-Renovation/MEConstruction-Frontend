805c7c5c380218bc4f3b22390184d4e88a3555b6
Xyron Uy <xyronuysoftware@gmail.com>
2026-03-18 12:55:52 +0800

    Version 20, fixing keep alive cron job

--- src/app/api/cron/keep-alive/route.js
+++ src/app/api/cron/keep-alive/route.js
@@ -15,22 +15,27 @@ export async function GET(request) {
     // 2. Perform a "Write" operation
     // Using an UPDATE is better than a SELECT because it's harder
     // for any layer to cache a database write.
-    const { error } = await supabase
-      .from("heartbeat") // Make sure this table exists!
-      .update({ last_ping: new Date().toISOString() })
-      .eq("id", 1);
+
+    // const { error } = await supabase
+    //   .from("heartbeat") // Make sure this table exists!
+    //   .update({ last_ping: new Date().toISOString() })
+    //   .eq("id", 1);
+
+    const { error } = await supabase.rpc("wake_up_db");
 
     if (error) throw error;
 
     return NextResponse.json({
       success: true,
-      message: "Supabase heartbeat updated",
+      // message: "Supabase heartbeat updated",
+      message: "Supabase RPC Heartbeat executed successfully",
     });
   } catch (err) {
     return NextResponse.json(
       {
         success: false,
-        error: err.message || "Error updating Supabase heartbeat.",
+        // error: err.message || "Error updating Supabase heartbeat.",
+        error: err.message || "Error executing Supabase RPC heartbeat.",
       },
       { status: 500 },
     );
